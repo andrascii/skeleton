605917c78bd15c793781a834eca1e90f0ef39ae1
Андрей Пугачев <apugachevdev@gmail.com>
2021-01-07 20:30:10 +0300

    Fixed building sanitized targets and coverage target on Mac OS

--- cmake/clang_options.cmake
+++ cmake/clang_options.cmake
@@ -1,16 +1,4 @@
-if (CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang")
-  find_program(LLVM_COV NAMES "llvm-cov" REQUIRED)
-  find_program(LLVM_PROFDATA NAMES "llvm-profdata" REQUIRED)
-
-  message(STATUS "LLVM_COV: ${LLVM_COV}")
-  message(STATUS "LLVM_PROFDATA: ${LLVM_PROFDATA}")
-
-  list(APPEND COVERAGE_FLAGS -fcoverage-mapping)
-  list(APPEND COVERAGE_FLAGS -fprofile-instr-generate)
-
-  list(APPEND SANITIZE_FLAGS "-fno-omit-frame-pointer")
-  list(APPEND SANITIZE_FLAGS "-fsanitize=address,undefined")
-
+if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang" OR CMAKE_CXX_COMPILER_ID STREQUAL "AppleClang")
   add_compile_options("$<$<CONFIG:DEBUG>:-O0>")
   add_compile_options("$<$<CONFIG:RELEASE>:-O3>")
   add_compile_options("-Wall")
